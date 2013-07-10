b32dfc6011f625ec92c1d6548216a364820647db
Henry Addo <addhen@linuxmail.org>
2013-07-10 09:49:13 +0900

    Add header file

---
+++ header.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Header template.
+ *
+ * @package P2
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="page_container">
+<div id="header">
+<?php do_action( 'before' ); ?>
+	<div class="sleeve">
+	<h1><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php if ( get_bloginfo( 'description' ) ) : ?>
+			<span><small><?php bloginfo( 'description' ); ?></small><span>
+		<?php endif; ?>
+		<a class="secondary" href="<?php echo home_url( '/' ); ?>"></a>
+
+		<?php if ( current_user_can( 'publish_posts' ) ) : ?>
+			<span><a href="" id="mobile-post-button" style="display: none;"><?php _e( 'Post', 'p2' ) ?></a></span>
+		<?php endif; ?>	
+	<!-- Google Public Location Badge -->
+<iframe src="http://www.google.com/latitude/apps/badge/api?user=3566689613755363308&type=iframe&maptype=roadmap&z=10" width="980" height="263" frameborder="0"></iframe>
+<!-- To disable location sharing, you *must* visit https://www.google.com/latitude/apps/badge and disable the Google Public Location badge. Removing this code snippet is not enough! -->
+	</div>
+
+	<?php if ( has_nav_menu( 'primary' ) ) : ?>
+	<div role="navigation" class="site-navigation main-navigation">
+		<h1 class="assistive-text"><?php _e( 'Menu', 'p2' ); ?></h1>
+		<div class="assistive-text skip-link"><a href="#main" title="<?php esc_attr_e( 'Skip to content', 'p2' ); ?>"><?php _e( 'Skip to content', 'p2' ); ?></a></div>
+
+		<?php wp_nav_menu( array(
+			'theme_location' => 'primary',
+			'fallback_cb'    => '__return_false',
+		) ); ?>
+	</div>
+	<?php endif; ?>
+</div>
+
+<div id="wrapper">
+
+	<?php get_sidebar(); ?>
